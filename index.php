ec587dc710b512b374deb7966c101dcfc5a942bf
Stefano <sverhoeve@davinci.nl>
2021-11-04 15:44:28 +0100

    added styling to uploadpage (index.php)

--- index.php
+++ index.php
@@ -23,43 +23,43 @@ if (empty($_SESSION['user']) || $_SESSION['user'] == null) {
     </a>
 
 <?php } else { ?>
-    <h1 class="text-6xl font-normal leading-normal mt-0 mb-2 text-blue-400">Showcase your work!</h1>
-    <h2>Upload your code...</h2>
-
-    <label class="w-64 flex flex-col items-center px-4 py-6 bg-white rounded-md shadow-md tracking-wide uppercase border border-blue cursor-pointer hover:bg-purple-600 hover:text-white text-purple-600 ease-linear transition-all duration-150">
-        <i class="fas fa-cloud-upload-alt fa-3x"></i>
-        <span class="mt-2 text-base leading-normal">Select a file</span>
-        <input type="file" class="hidden"/>
-    </label>
-
-    <label for="description">Description</label><br>
-    <textarea id="description" name="description" maxlength="140" rows="15" style="width:100%"></textarea><br>
-
-
-    <button onclick="transmitMessage('codeBlock')">Upload</button>
 
 <?php } ?>
 
 <div>
     <div class="text-black">
         <section class="text-gray-600 body-font">
-            <div class="max-w-7xl mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-                <div class="lg:flex-grow md:w-1/2 md:ml-24 pt-6 flex flex-col md:items-start md:text-left mb-40 items-center text-center">
+            <div class="max-w-7xl mx-auto flex px-5 py-12 md:py-36 md:flex-row flex-col items-center">
+                <div class="md:w-1/2 md:ml-24 pt-6 flex flex-col md:items-start md:text-left mb-20 items-center text-center">
                     <h1 class="mb-5 sm:text-6xl text-5xl items-center Avenir xl:w-2/2 text-gray-900">
                         Showcase your work</h1>
                     <p class="mb-4 xl:w-3/4 text-gray-600 text-lg">Here you can upload a screenshot or image of your
                         work. Work in progress or a finished product!</p>
-                    <div class="flex justify-center"><a
-                                class="inline-flex items-center px-5 py-3 mt-2 font-medium text-white transition duration-500 ease-in-out transform bg-transparent border rounded-lg bg-gray-900"
-                                href="pages/dashboard.php"><span class="justify-center">View other's work</span></a>
+                    <p class="mb-4 xl:w-3/4 text-gray-600 text-lg">Upload a file and optionally add a description.</p>
+
+                    <label class="mb-2 text-left text-gray-500 w-3/4 md:w-full xl:w-3/4 xl:self-start md:self-center"
+                           for="description">Description</label>
+                    <textarea
+                            class="form-textarea resize-none p-1 mb-4 border w-3/4 md:w-full xl:w-3/4 xl:self-start md:self-center"
+                            id="description" placeholder="Type your description here..." name="description" maxlength="140" rows="3"></textarea>
+
+                    <div class="flex justify-center">
+                        <a class="inline-flex items-center px-5 py-3 mt-2 font-medium text-white transition duration-500 ease-in-out transform bg-transparent border rounded-lg bg-gray-900"
+                           href="pages/dashboard.php">
+                            <span class="justify-center">Send it to the screen!</span>
+                        </a>
                     </div>
                 </div>
-                <div class="xl:mr-44 sm:mr-0 sm:mb-28 mb-0 lg:mb-0 mr-48 md:pl-10">
-                    <img class="w-100 md:ml-1 ml-24" alt="monitor" src="public/img/monitor.png">
+                <div class="md:mr-24 p-10 pb-28 md:ml-5 bg-contain bg-no-repeat"
+                     style="background-image:url('public/img/monitor.png');">
+                    <label class="w-64 flex flex-col items-center px-4 py-6 bg-white rounded-md shadow-md tracking-wide uppercase border border-blue cursor-pointer hover:bg-purple-600 hover:text-white text-purple-600 ease-linear transition-all duration-150">
+                        <i class="fas fa-cloud-upload-alt fa-3x"></i>
+                        <span class="mt-2 text-base leading-normal">Select a file</span>
+                        <input type="file" class="hidden"/>
+                    </label>
                 </div>
             </div>
         </section>
-
     </div>
 </div>
 
