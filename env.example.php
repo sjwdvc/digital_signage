c25d149c7641758e54b3cdb36eba4f4ac9cb0c76
Stefano <sverhoeve@davinci.nl>
2021-11-10 12:38:33 +0100

    made the final env.example for prod

--- env.example.php
+++ env.example.php
@@ -1,23 +1,29 @@
 <?php
-$tenant = 'YOUR TENANT CODE HERE';
+$tenant = '';
 
 $variables = [
-    'DB_HOST' => 'localhost',
+    'DB_TYPE' => 'mysql',
+    'DB_HOST' => '127.0.0.1',
+    'DB_CHARSET' => 'utf8',
     'DB_USERNAME' => 'root',
     'DB_PASSWORD' => '',
-    'DB_NAME' => 'digital_signage',
+    'DB_NAME' => '',
     'DB_PORT' => '3306',
 
-    'domain' => 'localhost',
+    'domain' => '',
     'wsPort' => '8080',
     'objectId' => '',
     'clientId' => '',
     'clientSecret' => '',
-    'redirectUri' => 'Call back route',
+    'redirectUri' => '',
+    'fetchUri' => '',
     'tenant' => '',
     'urlAuthorize' => 'https://login.microsoftonline.com/'. $tenant .'/oauth2/v2.0/authorize?',
     'urlAccessToken' => 'https://login.microsoftonline.com/'. $tenant .'/oauth2/v2.0/token',
     'urlResourceOwnerDetails' => 'https://graph.microsoft.com/v1.0/me',
+
+    'uploadFolder' => 'uploads/',
+    'uploadUrl' => ''
 
 ];
 
