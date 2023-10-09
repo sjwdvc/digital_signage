311cea08449ae96ac0eef360fe42654e208afae9
Stefano Verhoeve <stefanoverhoeve@Stefanos-MacBook-Pro.local>
2023-10-09 08:22:23 +0200

    updated example env

--- env.example.php
+++ env.example.php
@@ -2,30 +2,39 @@
 $tenant = '';
 
 $variables = [
+    //Domain
+    'domain' => '',
+    'wsPort' => '',
+    'subFolder' => '',
+    'appFolder' => '',
+
+    //upload
+    'uploadFolder' => '/uploads',
+
+    //DB
     'DB_TYPE' => 'mysql',
-    'DB_HOST' => '127.0.0.1',
+    'DB_HOST' => '',
     'DB_CHARSET' => 'utf8',
-    'DB_USERNAME' => 'root',
+    'DB_USERNAME' => '',
     'DB_PASSWORD' => '',
     'DB_NAME' => '',
     'DB_PORT' => '3306',
 
-    'domain' => '',
-    'wsPort' => '8080',
-    'objectId' => '',
+    //Oath2
     'clientId' => '',
     'clientSecret' => '',
-    'redirectUri' => '',
-    'fetchUri' => '',
     'tenant' => '',
-    'urlAuthorize' => 'https://login.microsoftonline.com/'. $tenant .'/oauth2/v2.0/authorize?',
-    'urlAccessToken' => 'https://login.microsoftonline.com/'. $tenant .'/oauth2/v2.0/token',
     'urlResourceOwnerDetails' => 'https://graph.microsoft.com/v1.0/me',
-
-    'uploadFolder' => 'uploads/',
-    'uploadUrl' => ''
-
 ];
+$variables['httpProtocol'] = $variables['domain'] != 'localhost' ? 'https://' : 'http://';
+
+$variables['uploadUrl'] = $variables['httpProtocol'] . $variables['domain'].$variables['subFolder'];
+$variables['fetchUri'] = $variables['httpProtocol'].$variables['domain'].$variables['subFolder'].$variables['appFolder'].'/app/handleUpload.php';
+
+
+$variables['urlAuthorize'] = 'https://login.microsoftonline.com/'. $variables['tenant'] .'/oauth2/v2.0/authorize?';
+$variables['urlAccessToken'] = 'https://login.microsoftonline.com/'. $variables['tenant'] .'/oauth2/v2.0/token';
+$variables['redirectUri'] = $variables['httpProtocol'] . $variables['domain'].$variables['subFolder'] .'/index.php';
 
 foreach ($variables as $key => $value) {
     putenv("$key=$value");
